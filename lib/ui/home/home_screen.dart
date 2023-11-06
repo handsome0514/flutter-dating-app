e3ce9d51f1484c360a3217ddc7df98354bbf0d6f
charmer1025 <78626326+charmer1025@users.noreply.github.com>
2023-11-05 21:27:51 -0500

    complete home page

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bematched/constants/color.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -25,6 +26,45 @@ class _HomeScreenState extends State<HomeScreen> {
               fit: BoxFit.cover,
             ),
           ),
+          child: Container(
+              child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Welcolme to',
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 40,
+                      color: Colors.white),
+                ),
+                Center(
+                  child: Image.asset("assets/images/logo.png"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Connecting Mongolians, One Swipe at a Time!',
+                  style: TextStyle(fontSize: 15, color: Colors.white),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    child: const Text(
+                      'Start',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: themeColor,
+                      minimumSize: const Size.fromHeight(50),
+                    ))
+              ],
+            ),
+          )),
         ),
       ),
     );
