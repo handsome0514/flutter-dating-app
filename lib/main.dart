36ba9f2732850b38ba7090d598b53be7d1abc954
dreamhigh0609 <luka.onishi0609@gmail.com>
2023-11-10 05:20:54 +0200

    made code clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:bematched/ui/home/home_screen.dart';
+import 'package:bematched/ui/welcome/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'BeMatched',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
